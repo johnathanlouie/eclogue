70affe40fa44c7f7496ed98bdb10aa420f2f7267
Johnathan Louie <johnathanlouie88@gmail.com>
2022-02-24 17:42:36 -0800

    Create WriterInterface.php

---
+++ src/WriterInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Lwd\Logger;
+
+/**
+ * Log writer for any output.
+ */
+interface WriterInterface
+{
+    /**
+     * Writes the log entry.
+     * 
+     * @param string $category
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public function write($category, $level, $message, $context);
+}
