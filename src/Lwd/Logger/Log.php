46e3e1eb28385e3d89a1b2e1a6b4c4a58b88143a
Johnathan Louie <johnathanlouie88@gmail.com>
2023-06-03 00:33:35 -0700

    Create Log.php

---
+++ src/Lwd/Logger/Log.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Lwd\Logger;
+
+use DateTime;
+use Exception;
+
+/**
+ * Data object for log entries.
+ *
+ * @author Johnathan Louie
+ */
+class Log {
+
+    /** @var string|null */
+    public $message = null;
+
+    /** @var string|null */
+    public $level = null;
+
+    /** @var DateTime|null */
+    public $timestamp = null;
+
+    /** @var Exception|null */
+    public $exception = null;
+
+    /** @var string|null */
+    public $serverAddress = null;
+
+    /** @var string|null */
+    public $clientAddress = null;
+
+    /** @var array */
+    public $customFields = [];
+
+}
