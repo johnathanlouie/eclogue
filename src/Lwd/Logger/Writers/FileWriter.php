d7b8a3c60abd518ce2639bd671771257ef8ecee5
Johnathan Louie <johnathanlouie88@gmail.com>
2023-06-02 23:19:51 -0700

    Update FileWriter.php

--- src/Lwd/Logger/Writers/FileWriter.php
+++ src/Lwd/Logger/Writers/FileWriter.php
@@ -2,42 +2,52 @@
 
 namespace Lwd\Logger\Writers;
 
-use DateTimeInterface;
-use Lwd\Logger\WriterInterface;
+use ErrorException;
+use Lwd\Logger\Log;
+use Lwd\Logger\StringWriterInterface;
 
 /**
  * Simple log writer for a monolithic file.
+ *
+ * @author Johnathan Louie
  */
-class FileWriter implements WriterInterface {
+class FileWriter implements StringWriterInterface {
 
-    /** @var string */
+    /** @var string File path to log file. */
     private $filename;
 
     /**
      * Constructs the log writer.
-     * 
+     *
      * @param string $filename The file to write to.
      */
-    public function __construct($filename = '/var/log/apache2/error.log') {
+    public function __construct($filename) {
         $this->filename = $filename;
     }
 
     /**
-     * Writes the log entry to a file.
-     * 
-     * @param string|null $category
-     * @param mixed $level
-     * @param string $message
-     * @param array $context
+     * Writes the log entry to the file.
+     *
+     * @param Log $log
+     * @param string $formatted Formatted string to be written.
      * @return void
+     * @throws ErrorException
      */
-    public function write($category, $level, $message, $context) {
-        $timestamp = date_create()->format(DateTimeInterface::RFC3339_EXTENDED);
-        $category = is_null($category) ? '' : "$category.";
-        $level = strtoupper($level);
-        $context = json_encode($context);
-        $entry = sprintf('[%s] %s%s: %s %s', $timestamp, $category, $level, $message, $context);
-        file_put_contents($this->filename, $entry, FILE_APPEND);
+    public function write($log, $formatted) {
+        try {
+            set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+                throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+            });
+
+            $byteCount = file_put_contents($this->filename, $formatted . PHP_EOL, FILE_APPEND);
+            if ($byteCount === false) {
+                throw new ErrorException("Failed to write log entry to '{$this->filename}'");
+            }
+        } catch (ErrorException $e) {
+            throw $e;
+        } finally {
+            restore_error_handler();
+        }
     }
 
 }
