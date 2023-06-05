5895bf3bf06a726028da70ee6dfd310fb6ee66be
Johnathan Louie <johnathanlouie88@gmail.com>
2023-06-05 01:40:08 -0700

    Ensures the parent directories exist before writing to the log file

--- src/Lwd/Logger/Writers/FileWriter.php
+++ src/Lwd/Logger/Writers/FileWriter.php
@@ -26,6 +26,23 @@ class FileWriter implements WriterInterface {
     }
 
     /**
+     * Creates the parent directories if they do not exist.
+     *
+     * @return void
+     * @throws Exception On failure to create directory.
+     */
+    private function mkdir() {
+        if (!is_file($this->filename)) {
+            $dirname = dirname($this->filename);
+            if (!is_dir($dirname)) {
+                if (!mkdir($dirname, 0777, true)) {
+                    throw new Exception("Failed to make '{$dirname}' directory");
+                }
+            }
+        }
+    }
+
+    /**
      * Writes the log entry to the file.
      *
      * @param string $payload Log entry as a formatted payload.
@@ -39,11 +56,12 @@ class FileWriter implements WriterInterface {
                 throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
             });
 
+            $this->mkdir();
             $byteCount = file_put_contents($this->filename, $payload . PHP_EOL, FILE_APPEND);
             if ($byteCount === false) {
                 throw new ErrorException("Failed to write log entry to '{$this->filename}'");
             }
-        } catch (ErrorException $e) {
+        } catch (Exception $e) {
             throw $e;
         } finally {
             restore_error_handler();
